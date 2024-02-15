addee3215494b87318188a0c5992b4710fd33687
tasneem94 <tasneemsakif31@gmail.com>
2024-02-15 21:04:35 +0600

    added more functions

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export const ToDoList = () => {
-  const tasks = ["apple", "mango", "banana"];
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
 
@@ -12,7 +11,8 @@ export const ToDoList = () => {
   const addTask = () => {
     const task = {
       taskName: newTask,
-      completed: false,
+      checked: false,
+      hidden: false,
     };
     if (newTask.trim() !== "") {
       setTodoList((prevTodoList) => [...prevTodoList, task]);
@@ -28,7 +28,7 @@ export const ToDoList = () => {
     setTodoList(
       todoList.map((task, i) => {
         if (index === i) {
-          return { ...task, completed: true };
+          return { ...task, checked: true };
         } else {
           return task;
         }
@@ -58,8 +58,32 @@ export const ToDoList = () => {
     }
   };
 
+  const showAllTask = () => {
+    setTodoList(todoList.map((task) => ({ ...task, hidden: false })));
+  };
+
+  const showCheckedTask = () => {
+    setTodoList(
+      todoList.map((task) =>
+        task.checked ? { ...task, hidden: false } : { ...task, hidden: true }
+      )
+    );
+  };
+
+  const showUncheckedTask = () => {
+    setTodoList(
+      todoList.map((task) =>
+        task.checked ? { ...task, hidden: true } : { ...task, hidden: false }
+      )
+    );
+  };
+
+  const clearCheckedTask = () => {
+    setTodoList(todoList.filter((task) => !task.checked));
+  };
+
   const clearAllTask = () => {
-    setTodoList(todoList.filter((task) => !task.completed));
+    setTodoList([]);
   };
 
   return (
@@ -72,46 +96,66 @@ export const ToDoList = () => {
           value={newTask}
           onChange={handleInputChange}
         />
-        <button className="add-task-btn" onClick={addTask}>
+        <button className="add-task-btn btn" onClick={addTask}>
           ADD
         </button>
-        <button className="celar-all-task-btn" onClick={clearAllTask}>
+        <button className="btn" onClick={showAllTask}>
+          SHOW ALL
+        </button>
+        <button className="btn" onClick={showCheckedTask}>
+          SHOW CHECKED
+        </button>
+        <button className="btn" onClick={showUncheckedTask}>
+          SHOW UNCHECKED
+        </button>
+        <button className="btn" onClick={clearCheckedTask}>
+          CLEAR CHECKED
+        </button>
+        <button className="btn" onClick={clearAllTask}>
           CLEAR ALL
         </button>
       </div>
       <div className="task-tile">
         <ul>
           {todoList.map((task, index) => {
-            return (
-              <li key={index}>
-                <span
-                  className="text"
-                  style={{
-                    textDecoration: task.completed ? "line-through" : "",
-                  }}
-                >
-                  {task.taskName}
-                </span>
-                <button className="del-btn" onClick={() => delTask(index)}>
-                  DEL
-                </button>
-                <button className="up-btn" onClick={() => moveTaskUp(index)}>
-                  UP
-                </button>
-                <button
-                  className="down-btn"
-                  onClick={() => moveTaskDown(index)}
-                >
-                  DOWN
-                </button>
-                <button
-                  className="chekced-btn"
-                  onClick={() => checkedTask(index)}
-                >
-                  CHECKED
-                </button>
-              </li>
-            );
+            if (!task.hidden) {
+              return (
+                <li key={index}>
+                  <span
+                    className="text"
+                    style={{
+                      textDecoration: task.checked ? "line-through" : "",
+                    }}
+                  >
+                    {task.taskName}
+                  </span>
+                  <button
+                    className="del-btn btn"
+                    onClick={() => delTask(index)}
+                  >
+                    DEL
+                  </button>
+                  <button
+                    className="up-btn btn"
+                    onClick={() => moveTaskUp(index)}
+                  >
+                    UP
+                  </button>
+                  <button
+                    className="down-btn btn"
+                    onClick={() => moveTaskDown(index)}
+                  >
+                    DOWN
+                  </button>
+                  <button
+                    className="chekced-btn btn"
+                    onClick={() => checkedTask(index)}
+                  >
+                    CHECKED
+                  </button>
+                </li>
+              );
+            }
           })}
         </ul>
       </div>
